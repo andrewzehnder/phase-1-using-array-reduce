4557c2291eb9852c8a80fa9b8633381487d0ae24
Andrew <azehnder2@gmail.com>
2022-04-19 15:17:21 -0700

    commit tree 4bae8b35aaeaa405c5a065ae6429f9708cb47ccb

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+// function assembledBatteries(batteryBatches, reducer, init) {
+//     let totalBatteries = init;
+//     for (const battery of batteryBatches) {
+//         totalBatteries = reducer(accum, battery);
+//     }
+//     return totalBatteries;
+// }
+
+const totalBatteries = batteryBatches.reduce((batteries, totalNumber) => batteries += totalNumber, 0);
